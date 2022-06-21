7d8e15a78529e2257724dd75d484ceffd333f274
Maria Zadnepryanets <mariya.z@gmail.com>
2022-06-21 22:38:15 +0200

    Add create, destory, update, show methods to the dogs controller

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -2,6 +2,50 @@ class Api::V1::DogsController < ApplicationController
   def index
     dogs = Dog.all
 
-    render json: dogs, include: [:breed, :company]
+    render json: dogs
+  end
+
+  def show
+    render json: dog
+  rescue ActiveRecord::RecordNotFound
+    render json: { 'error': 'Record not found' }
+  end
+
+  def create
+    breed = Breed.find(params[:breed])
+    company = Company.find(params[:company])
+    dog = Dog.create!(name: params[:name], breed: breed, company: company)
+
+    render json: dog
+  rescue ActiveRecord::RecordInvalid
+    render json: { 'error': 'Unprocessable entry' }
+  rescue ActiveRecord::RecordNotFound
+    render json: { 'error': 'Record not found' }
+  end
+
+  def update
+    if dog.update!(update_dog_params)
+      render json: dog
+    else
+      render json: { 'error': 'Dog not found' }
+    end
+  end
+
+  def destroy
+    dog.destroy!
+
+    render json: { 'message': 'Dog is deleted'}
+  rescue ActiveRecord::RecordNotFound
+    render json: { 'error': 'Record not found' }
+  end
+
+  private
+
+  def dog
+    @dog ||= Dog.find(params[:id])
+  end
+
+  def update_dog_params
+    params.permit(:name)
   end
 end
